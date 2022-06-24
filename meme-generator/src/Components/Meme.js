023cebeeef180dc57f1518cce8380b55d0ef860d
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-06-24 12:12:35 -0700

    import meme data and generate function which will select random meme

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+
+  function getMemeImage() {
+    const memeArray = memesData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memeArray.length);
+    console.log(memeArray[randomIndex].url);
+  }
+
   return (
     <div className='container'>
-      <form >
+      <div className='form'>
         <div className='form-inputs'>
           <input type="text" placeholder='Shut up' className='form-input-left'></input>
           <input type="text" placeholder='and take my money' className='form-input-right'></input>
         </div>
-        <button>Get a new meme image 🖼</button>
-      </form>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
+      </div>
 
     </div>
   )
